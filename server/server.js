22abb2d707a2d57b05d364221ce4912b86b030f5
Peter Wang <peterw712@outlook.com>
2021-07-16 11:06:45 -0400

    bug fix

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers, 
